5a00146cda4afecd2d23932dc88e64fcddb645c0
theshekhargupta <theshekhargupta@gmail.com>
2020-02-18 19:14:55 +0530

    Disabling iOS SDK

--- userexperior-cordova-plugin/src/ios/CDVUserExperior.m
+++ userexperior-cordova-plugin/src/ios/CDVUserExperior.m
@@ -4,107 +4,107 @@
 @implementation CDVUserExperior
 
 - (void)startRecording:(CDVInvokedUrlCommand *)command {
-    CDVPluginResult* pluginResult = nil;
-    NSString* apiKey = command.arguments[0];
-    
-    if (apiKey.length > 0) {
-        NSLog(@"UserExperior 4.1.29 initiated with given key %@ via cordova", apiKey);
-        [UserExperior initialize:apiKey];
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
-    } else {
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
-    }
-    
-    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+//    CDVPluginResult* pluginResult = nil;
+//    NSString* apiKey = command.arguments[0];
+//    
+//    if (apiKey.length > 0) {
+//        NSLog(@"UserExperior 4.1.29 initiated with given key %@ via cordova", apiKey);
+//        [UserExperior initialize:apiKey];
+//        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+//    } else {
+//        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
+//    }
+//    
+//    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
 
-- (void)setCustomTag:(CDVInvokedUrlCommand*)command {
-    CDVPluginResult *pluginResult = nil;
-    NSString *tag = command.arguments[0];
-    NSString *type = command.arguments[1];
-
-    if (tag.length>0 && type.length>0) {
-        [UserExperior setCustomTagWithString:tag customType:type];
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
-    } else {
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
-    }
-    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-}
-
-- (void)setUserIdentifier:(CDVInvokedUrlCommand*)command {
-    CDVPluginResult* pluginResult = nil;
-    NSString *identifier = command.arguments[0];
-
-    if (identifier.length > 0) {
-        [UserExperior setUserIdentifier:identifier];
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
-    } else {
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
-    }
-
-    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-}
-
-- (void)startScreen:(CDVInvokedUrlCommand*)command {
-    CDVPluginResult* pluginResult = nil;
-    NSString *screenName = command.arguments[0];
-    
-    if (screenName.length > 0) {
-        [UserExperior startScreen:screenName];
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
-    } else {
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
-    }
-    
-    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-}
-    
-- (void)resumeRecording:(CDVInvokedUrlCommand*)command {
-    [UserExperior resumeRecording];
-
-    [self.commandDelegate sendPluginResult:[CDVPluginResult resultWithStatus:CDVCommandStatus_OK] callbackId:command.callbackId];
-}
-
-- (void)pauseRecording:(CDVInvokedUrlCommand*)command {
-    [UserExperior pauseRecording];
-
-    [self.commandDelegate sendPluginResult:[CDVPluginResult resultWithStatus:CDVCommandStatus_OK] callbackId:command.callbackId];
-}
-
-- (void)stopRecording:(CDVInvokedUrlCommand*)command {
-    [UserExperior stopRecording];
-
-    [self.commandDelegate sendPluginResult:[CDVPluginResult resultWithStatus:CDVCommandStatus_OK] callbackId:command.callbackId];
-}
-
-- (void)consent:(CDVInvokedUrlCommand*)command {
-    [UserExperior consent];
-    
-    [self.commandDelegate sendPluginResult:[CDVPluginResult resultWithStatus:CDVCommandStatus_OK] callbackId:command.callbackId];
-}
-
-- (void)optIn:(CDVInvokedUrlCommand*)command {
-    [UserExperior optIn];
-    
-    [self.commandDelegate sendPluginResult:[CDVPluginResult resultWithStatus:CDVCommandStatus_OK] callbackId:command.callbackId];
-}
-
-- (void)optOut:(CDVInvokedUrlCommand*)command {
-    [UserExperior optOut];
-    
-    [self.commandDelegate sendPluginResult:[CDVPluginResult resultWithStatus:CDVCommandStatus_OK] callbackId:command.callbackId];
-}
-
-- (BOOL)getOptOutStatus:(CDVInvokedUrlCommand*)command {
-    if ([UserExperior getOptOutStatus]) {
-        [self.commandDelegate sendPluginResult:[CDVPluginResult resultWithStatus:CDVCommandStatus_OK] callbackId:command.callbackId];
-        return TRUE;
-    } else {
-        [self.commandDelegate sendPluginResult:[CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR] callbackId:command.callbackId];
-        return FALSE;
-    }
-}
+//- (void)setCustomTag:(CDVInvokedUrlCommand*)command {
+//    CDVPluginResult *pluginResult = nil;
+//    NSString *tag = command.arguments[0];
+//    NSString *type = command.arguments[1];
+//
+//    if (tag.length>0 && type.length>0) {
+//        [UserExperior setCustomTagWithString:tag customType:type];
+//        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+//    } else {
+//        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
+//    }
+//    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+//}
+//
+//- (void)setUserIdentifier:(CDVInvokedUrlCommand*)command {
+//    CDVPluginResult* pluginResult = nil;
+//    NSString *identifier = command.arguments[0];
+//
+//    if (identifier.length > 0) {
+//        [UserExperior setUserIdentifier:identifier];
+//        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+//    } else {
+//        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
+//    }
+//
+//    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+//}
+//
+//- (void)startScreen:(CDVInvokedUrlCommand*)command {
+//    CDVPluginResult* pluginResult = nil;
+//    NSString *screenName = command.arguments[0];
+//
+//    if (screenName.length > 0) {
+//        [UserExperior startScreen:screenName];
+//        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+//    } else {
+//        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
+//    }
+//
+//    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+//}
+//
+//- (void)resumeRecording:(CDVInvokedUrlCommand*)command {
+//    [UserExperior resumeRecording];
+//
+//    [self.commandDelegate sendPluginResult:[CDVPluginResult resultWithStatus:CDVCommandStatus_OK] callbackId:command.callbackId];
+//}
+//
+//- (void)pauseRecording:(CDVInvokedUrlCommand*)command {
+//    [UserExperior pauseRecording];
+//
+//    [self.commandDelegate sendPluginResult:[CDVPluginResult resultWithStatus:CDVCommandStatus_OK] callbackId:command.callbackId];
+//}
+//
+//- (void)stopRecording:(CDVInvokedUrlCommand*)command {
+//    [UserExperior stopRecording];
+//
+//    [self.commandDelegate sendPluginResult:[CDVPluginResult resultWithStatus:CDVCommandStatus_OK] callbackId:command.callbackId];
+//}
+//
+//- (void)consent:(CDVInvokedUrlCommand*)command {
+//    [UserExperior consent];
+//
+//    [self.commandDelegate sendPluginResult:[CDVPluginResult resultWithStatus:CDVCommandStatus_OK] callbackId:command.callbackId];
+//}
+//
+//- (void)optIn:(CDVInvokedUrlCommand*)command {
+//    [UserExperior optIn];
+//
+//    [self.commandDelegate sendPluginResult:[CDVPluginResult resultWithStatus:CDVCommandStatus_OK] callbackId:command.callbackId];
+//}
+//
+//- (void)optOut:(CDVInvokedUrlCommand*)command {
+//    [UserExperior optOut];
+//
+//    [self.commandDelegate sendPluginResult:[CDVPluginResult resultWithStatus:CDVCommandStatus_OK] callbackId:command.callbackId];
+//}
+//
+//- (BOOL)getOptOutStatus:(CDVInvokedUrlCommand*)command {
+//    if ([UserExperior getOptOutStatus]) {
+//        [self.commandDelegate sendPluginResult:[CDVPluginResult resultWithStatus:CDVCommandStatus_OK] callbackId:command.callbackId];
+//        return TRUE;
+//    } else {
+//        [self.commandDelegate sendPluginResult:[CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR] callbackId:command.callbackId];
+//        return FALSE;
+//    }
+//}
 
 @end
 
