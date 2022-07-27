08f400457f9293b898dec23a18d5b84097815f3c
Zeeshan Ali Sayyed <zeeshan@userexperior.com>
2022-07-27 21:48:43 +0530

    fixed abort and signal crashes, fixed ltfs crashes in android SDK; updated ue cordova plugin version to 1.4.7 in package.json, plugin.xml and js file;

--- userexperior-cordova-plugin/www/UserExperiorPlugin.js
+++ userexperior-cordova-plugin/www/UserExperiorPlugin.js
@@ -1,7 +1,7 @@
 var exec = require('cordova/exec');
 
 const fw = "ca"; // framework: cordova
-const sv = "1.4.6"; // SDK/Plugin Version
+const sv = "1.4.7"; // SDK/Plugin Version
 
 var UserExperiorPlugin = function() {};
 
