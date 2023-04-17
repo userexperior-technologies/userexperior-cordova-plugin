caef4437a7f0286c2d997f24ad7e7466c2677198
Shekhar Gupta <shekhar@userexperior.com>
2023-04-17 19:10:44 +0530

    iOS change for Webview masking

--- userexperior-cordova-plugin/www/UserExperiorPlugin.js
+++ userexperior-cordova-plugin/www/UserExperiorPlugin.js
@@ -1,7 +1,7 @@
 var exec = require('cordova/exec');
 
 const fw = "ca"; // framework: cordova
-const sv = "1.5.1"; // SDK/Plugin Version
+const sv = "2.0.0"; // SDK/Plugin Version
 
 var UserExperiorPlugin = function() {};
 
